50a90f92d82abdb9dd143d23dfd71cf5ff78eacd
Zekoq24 <aaabb775180870@gmail.com>
2025-06-28 21:59:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.template_folder = 'templates'
 logging.basicConfig(level=logging.INFO)
 
-RPC_URL = "https://proud-aged-flower.solana-devnet.quiknode.pro/6c4369466a2cfc21c12af4a500501aa9b0093340"
+RPC_URL = "https://solana-mainnet.g.alchemy.com/v2/QMBCCev_Ig1zGFssTed57KsriUzCryCj"
 
 @app.route('/')
 def home():
